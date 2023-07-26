09f3e36ed22211bbd957cc0486fb90fa01ffc94b
Fredlike <87642054+Fredlike@users.noreply.github.com>
2023-07-26 21:37:39 +0200

    trying new script

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -1,4 +1,3 @@
-// JavaScript code to handle user input and API calls
 const chatLog = document.getElementById("chat-log");
 const inputText = document.getElementById("input-text");
 const sendButton = document.getElementById("send-button");
@@ -27,28 +26,27 @@ async function sendMessage() {
   displayMessage(question, true);
   inputText.value = "";
 
-  fetch("/api/gpt", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "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", // Replace with your actual API key
-    },
-    body: JSON.stringify({ question }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const answer = data.answer;
-      typeWriterEffect(answer); // Show the response using typewriter effect
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      displayMessage("Sorry, an error occurred while processing your request.");
+  try {
+    const response = await fetch("/api/gpt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "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", // Replace with your actual API key
+      },
+      body: JSON.stringify({ question }),
     });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    const answer = data.answer;
+    typeWriterEffect(answer); // Show the response using typewriter effect
+  } catch (error) {
+    console.error("Error:", error);
+    displayMessage("Sorry, an error occurred while processing your request.");
+  }
 }
 
 function clearChat() {
